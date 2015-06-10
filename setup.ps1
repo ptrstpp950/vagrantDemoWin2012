d9b56d0919d3304f76b831a234a625d05b83d678
Mateusz Janiszewski <Mateusz.Janiszewski@partners.mbank.pl>
2015-06-10 13:48:10 +0200

    zmiany do Vagrantfile i setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,4 @@
+mkdir -Force .\resources
 pushd .\resources
 if(!(Test-Path -Path .\NDP452-KB2901907-x86-x64-AllOS-ENU.exe )){
 	curl http://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe -OutFile NDP452-KB2901907-x86-x64-AllOS-ENU.exe
